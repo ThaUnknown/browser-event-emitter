c1f5d1945ba25ba739dd757a5701473ff3a4eeb1
ThaUnknown <6506529+ThaUnknown@users.noreply.github.com>
2022-04-23 14:45:41 +0200

    first implem

---
+++ index.js
@@ -0,0 +1,26 @@
+EventTarget.prototype.on = function (type, listener, options) {
+  if (typeof type !== 'string' || typeof listener !== 'function') return null
+  this.addEventListener(type, listener, options)
+}
+
+EventTarget.prototype.once = function (type, listener, options) {
+  if (typeof type !== 'string' || typeof listener !== 'function') return null
+  this.addEventListener(type, listener, { ...options, once: true })
+}
+
+EventTarget.prototype.emit = function (type, data) {
+  if (typeof type !== 'string') return null
+  // emit user specified event object, or a custom event if data is specified, or just an event when there's no data
+  this.dispatchEvent(data instanceof Event ? data : data !== undefined ? new CustomEvent(type, { detail: data }) : new Event(type))
+}
+
+EventTarget.prototype.removeListener = function (type, listener) {
+  if (typeof type !== 'string' || typeof listener !== 'function') return null
+  this.removeEventListener(type, listener)
+}
+
+if (!globalThis.EventEmitter) globalThis.EventEmitter = EventTarget
+
+const exp = EventTarget
+
+export { exp as EventEmitter }
